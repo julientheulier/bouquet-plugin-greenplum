92d99360181b7b9b681a3f6295a55482436b057e
olivier <obalbous@squidsolutions.com>
2016-02-01 15:48:04 +0100

    Open Bouquet initial release

---
+++ src/com/squid/core/jdbc/vendor/greenplum/GreenplumMetadataSupport.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright © Squid Solutions, 2016
+ *
+ * This file is part of Open Bouquet software.
+ *  
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * There is a special FOSS exception to the terms and conditions of the 
+ * licenses as they are applied to this program. See LICENSE.txt in
+ * the directory of this program distribution.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Squid Solutions also offers commercial licenses with additional warranties,
+ * professional functionalities or services. If you purchase a commercial
+ * license, then it supersedes and replaces any other agreement between
+ * you and Squid Solutions (above licenses and LICENSE.txt included).
+ * See http://www.squidsolutions.com/EnterpriseBouquet/
+ *******************************************************************************/
+package com.squid.core.jdbc.vendor.greenplum;
+
+import com.squid.core.jdbc.vendor.greenplum.postgresql.PostgresqlMetadataSupport;
+
+public class GreenplumMetadataSupport extends PostgresqlMetadataSupport {
+
+    private String[] systemSchemas = new String[]{"gp_toolkit","information_schema","pg_aoseg","pg_bitmapindex","pg_catalog"};
+    
+    @Override
+    public boolean isSystemSchema(String name) {
+    	for (int i=0;i<systemSchemas.length;i++) {
+    		if (systemSchemas[i].equalsIgnoreCase(name)) return true;
+    	}
+    	// else
+    	return false;
+    }
+
+}
---
+++ src/com/squid/core/jdbc/vendor/greenplum/GreenplumSQLSkin.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright © Squid Solutions, 2016
+ *
+ * This file is part of Open Bouquet software.
+ *  
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * There is a special FOSS exception to the terms and conditions of the 
+ * licenses as they are applied to this program. See LICENSE.txt in
+ * the directory of this program distribution.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Squid Solutions also offers commercial licenses with additional warranties,
+ * professional functionalities or services. If you purchase a commercial
+ * license, then it supersedes and replaces any other agreement between
+ * you and Squid Solutions (above licenses and LICENSE.txt included).
+ * See http://www.squidsolutions.com/EnterpriseBouquet/
+ *******************************************************************************/
+package com.squid.core.jdbc.vendor.greenplum;
+
+import com.squid.core.database.model.DatabaseProduct;
+import com.squid.core.jdbc.vendor.greenplum.postgresql.render.PostgresSQLSkin;
+import com.squid.core.sql.db.templates.ISkinProvider;
+
+
+public class GreenplumSQLSkin extends PostgresSQLSkin {
+
+	public GreenplumSQLSkin(ISkinProvider provider, DatabaseProduct product) {
+		super(provider, product);
+	}
+	
+	@Override
+	public boolean isShareNothing() {
+		return true;
+	}
+	
+}
