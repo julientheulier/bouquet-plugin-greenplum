6caf28dc203800c59373c6e804cf104a8dcc4225
hoahd <hoa@squidsolutions.com>
2016-12-14 16:33:24 +0100

    T1828

--- src/com/squid/core/jdbc/vendor/greenplum/GreenplumBouquetPlugin.java
+++ src/com/squid/core/jdbc/vendor/greenplum/GreenplumBouquetPlugin.java
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * Copyright © Squid Solutions, 2016
+ *
+ * This file is part of Open Bouquet software.
+ *  
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * There is a special FOSS exception to the terms and conditions of the 
+ * licenses as they are applied to this program. See LICENSE.txt in
+ * the directory of this program distribution.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Squid Solutions also offers commercial licenses with additional warranties,
+ * professional functionalities or services. If you purchase a commercial
+ * license, then it supersedes and replaces any other agreement between
+ * you and Squid Solutions (above licenses and LICENSE.txt included).
+ * See http://www.squidsolutions.com/EnterpriseBouquet/
+ *******************************************************************************/
+
 package com.squid.core.jdbc.vendor.greenplum;
 
 import java.net.URL;
@@ -7,12 +31,14 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
+import com.squid.core.database.impl.DefaultDriverShim;
+import com.squid.core.database.impl.DriverShim;
 import com.squid.core.database.plugins.BaseBouquetPlugin;
 
 public class GreenplumBouquetPlugin extends BaseBouquetPlugin {
 
-	ArrayList<Driver> exposedDrivers ;
-	
+	ArrayList<Driver> exposedDrivers;
+
 	@Override
 	public void loadDriver() {
 		// get the current plugin jar
@@ -28,20 +54,22 @@ public class GreenplumBouquetPlugin extends BaseBouquetPlugin {
 		ServiceLoader<Driver> sl = ServiceLoader.load(java.sql.Driver.class, driverCL);
 		Iterator<Driver> driversIter = sl.iterator();
 		this.drivers = new ArrayList<Driver>();
-		this.exposedDrivers= new  ArrayList<Driver>();
 		while (driversIter.hasNext()) {
-			Driver next = driversIter.next(); 
+			Driver next = driversIter.next();
 			drivers.add(next);
-			if (!next.getClass().toString().contains("postgres")){
-				this.exposedDrivers.add(next);
-			}
 		}
 		Thread.currentThread().setContextClassLoader(rollback);
 	}
-	
+
 	@Override
-	public ArrayList<Driver> getDrivers(){
-		return this.exposedDrivers;
+	public ArrayList<DriverShim> getDrivers() {
+		ArrayList<DriverShim> shims = new ArrayList<DriverShim>();
+		for (Driver d : this.drivers) {
+			if (!d.getClass().toString().contains("postgres")) {
+				shims.add(new DefaultDriverShim(d));
+			}
+		}
+		return shims;
 	}
 
 }
